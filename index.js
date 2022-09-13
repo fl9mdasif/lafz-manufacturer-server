f17f14068d8b51b7f3dadc765c48435f7eb3c58a
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2022-09-13 21:11:07 +0600

    delete user

--- index.js
+++ index.js
@@ -210,9 +210,10 @@ async function run() {
         });
 
         // delete a user  
-        app.delete('/users/:email', async (req, res) => {
-            const email = req.params.email;
-            const result = await userEmailCollection.deleteOne(email);
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userEmailCollection.deleteOne(query);
             res.send(result);
         });
         //delete a shoe by user
